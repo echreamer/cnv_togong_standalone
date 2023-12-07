631c7bda8de994316fe49ad6897e0e127a917f57
prollogger <prollogger@gmail.com>
2023-12-07 16:59:20 +0900

    pull

--- test.py
+++ test.py
@@ -1,25 +1,3 @@
-
-import ezdxf
-
-
-def load_dxf_layers(dxf_file_path):
-    try:
-
-        doc = ezdxf.readfile(dxf_file_path)
-
-        layers = [layer.dxf.name for layer in doc.layers]
-        return layers
-    except IOError:
-        print(f"Cannot open file: {dxf_file_path}")
-        return []
-    except ezdxf.DXFStructureError:
-        print(f"Invalid or corrupted DXF file: {dxf_file_path}")
-        return []
-
-
-example_dxf_path = r"C:\C00-100.dxf"
-layer_list = load_dxf_layers(example_dxf_path)
-print(layer_list)
-print('test')
-
-
+import sys
+sys.path.append(r"C:\Program Files\FreeCAD 0.21\lib")  # FreeCAD 라이브러리 경로
+import FreeCAD, Part
